2d1c414f5cabc58b1202c5699bad04da74fcc89f
Yehya0 <yehyaa@umich.edu>
2022-10-20 18:41:08 -0400

    changed n umbers

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 50;
-		int b = 50;
-		int c = 50;
+		int a = 5;
+		int b = 9;
+		int c = 4;
 		if (b*b - 4*a*c <0)
 			System.out.println("Thgjgis equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
