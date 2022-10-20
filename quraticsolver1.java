4fedd5922d227e879006336efb8a449ca60776b4
Yehya0 <yehyaa@umich.edu>
2022-10-20 18:22:25 -0400

    changed abc integers

--- quraticsolver1.java
+++ quraticsolver1.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5;
-		int b = 5;
-		int c = 5;
+		int a = 50;
+		int b = 50;
+		int c = 50;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
